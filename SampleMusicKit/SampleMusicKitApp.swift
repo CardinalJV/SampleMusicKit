366f32b0b85ff577fc3d9c8972cd19d2faf64ce2
CardinalJV <129071629+CardinalJV@users.noreply.github.com>
2026-05-18 01:05:24 +0200

    Initial Commit

---
+++ SampleMusicKit/SampleMusicKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleMusicKitApp.swift
+//  SampleMusicKit
+//
+//  Created by Viranaiken Jessy on 18/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct SampleMusicKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
